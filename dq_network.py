ff2adc34cbf9151681d83d9303709cc6006269b4
SanatBhargava <52417436+SanatBhargava@users.noreply.github.com>
2021-10-27 00:16:29 +0530

    Add files via upload

---
+++ dq_network.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import optimizers
+
+LOSS = "mean_squared_error"
+OPTIMIZER = optimizers.RMSprop(lr=0.00025, rho=0.95, epsilon=0.01)
+
+
+class DQNetwork:
+    def __init__(self, stacked_frame_dim, num_actions):
+        """
+        :param frame_dim: The dimension of the given frames.
+        :param num_actions: The number of possible actions
+        """
+        self.stacked_frame_dim = stacked_frame_dim
+        self.num_actions = num_actions
+
+        # this architecture is the same as in the deep mind atari paper "Playing Atari with Deep Reinforcement Learning"
+        self.model = Sequential()
+        self.model.add(layers.Conv2D(input_shape=stacked_frame_dim, filters=32, kernel_size=3, strides=2,
+                                     activation="relu"))
+        self.model.add(layers.Conv2D(filters=64, kernel_size=3, strides=1, activation="relu"))
+        self.model.add(layers.Conv2D(filters=64, kernel_size=3, strides=1, activation="relu"))
+        self.model.add(layers.Flatten())
+        self.model.add(layers.Dense(1024, activation="relu"))
+        self.model.add(layers.Dense(num_actions))
+        self.model.compile(loss=LOSS, optimizer=OPTIMIZER, metrics=["accuracy"])
---
+++ replay_memory.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 21 19:33:30 2020
+
+@author: sanat
+"""
+
+import random
+
+
+class ReplayMemory(object):
+    """Replay memory for learning."""
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, state, action, reward, next_state, done):
+        """Adds a transition to the replay memory."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = (state, action, reward, next_state, done)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        """Sample transitions at random."""
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
